f452fdfe34acff27278335f5a6e37cec81301684
Hugo Brucker <Hugo.Brucker@grenoble-inp.org>
2024-05-30 15:28:02 +0200

    Bug correction that makes extra copy

--- tp3/vector.cxx
+++ tp3/vector.cxx
@@ -1,5 +1,7 @@
 #include "vector.hxx"    
 #include <cassert>
+#include <cmath>
+
 using namespace std;
 
 Vector::Vector(vector<double> x, unsigned int dim){
@@ -8,46 +10,67 @@ Vector::Vector(vector<double> x, unsigned int dim){
     this->dim_ = dim;
 }
 
-Vector::~Vector(){}
-
-Vector& Vector::operator*(double lamda){
-    for (size_t i = 0; i < this->dim_; i++)
-    {
-        this->x_[i] *= lamda;
-    }
-    return *this;
+Vector::Vector(const Vector &v) {
+    this->x_ = v.x_;
+    this->dim_ = v.dim_;
 }
 
-Vector& Vector::operator+(const Vector &v){
-    assert(v.dim_ == this->dim_);
+Vector::Vector() : dim_(0) {}
+
+Vector::~Vector(){}
+
+Vector Vector::operator*(double lamda){
+    Vector result = *this;
     for (size_t i = 0; i < this->dim_; i++)
     {
-        this->x_[i] += v.x_[i];
+        result.setX(i, result.getX(i)*lamda);
     }
-    return *this;
+    return result;
+}
+
+Vector Vector::operator+(const Vector &v){
+    assert(v.dim_ == this->dim_);
+    Vector result = *this;
+    for (size_t i = 0; i < this->dim_; i++)
+    {
+        result.setX(i, result.getX(i)+v.getX(i));
+    }
+    return result;
 }
 
 Vector& Vector::operator=(const Vector &v){
-    this->x_   = v.x_;
-    this->dim_ = v.dim_;
-    return *this;
-}
-
-Vector& Vector::operator-(const Vector &v){
-    assert(v.dim_ == this->dim_);
-    for (size_t i = 0; i < this->dim_; i++){
-        this->x_[i] -= v.x_[i];
+    if (this != &v){   
+        this->x_   = v.x_;
+        this->dim_ = v.dim_;
     }
     return *this;
 }
 
+Vector Vector::operator-(const Vector &v){
+    assert(v.dim_ == this->dim_);
+    Vector result = *this;
+    for (size_t i = 0; i < this->dim_; i++){
+        result.setX(i, result.getX(i)-v.getX(i));
+    }
+    return result;
+}
+
 Vector& Vector::operator+=(const Vector &v){
-    return *this + v;
+    assert(v.getDim()==this->getDim());
+    for (size_t i = 0; i < this->getDim(); i++)
+    {
+        this->setX(i, this->getX(i)+v.getX(i));
+    }
+    return *this;
 }
 
 Vector& Vector::operator-=(const Vector &v){
-    return *this - v;
-}
+    assert(v.getDim()==this->getDim());
+    for (size_t i = 0; i < this->getDim(); i++)
+    {
+        this->setX(i, this->getX(i)-v.getX(i));
+    }
+    return *this;}
 
 bool Vector::operator==(const Vector &v) const {
     if (this->dim_ != v.dim_)
@@ -87,36 +110,46 @@ Vector operator*(double lamda, const Vector &v){
     return result;
 }
 
-int main()
-{
-    vector<double> x;
-    x.push_back(1); x.push_back(2);
-    Vector test  = Vector(x, 2);
-    Vector test2 = 10*test;
-    Vector test3 = test + test2;
-    Vector test4 = test - test;
-    cout << test;
-    cout << test2;
-    cout << test3;
-    cout << test4;
-    if (test4==test)
+double Vector::distance(Vector *v){
+    double dst = 0;
+    for (size_t i = 0; i < this->dim_; i++)
     {
-        cout << "true" << endl;
+        dst += (this->x_[i]-v->getX(i))*(this->x_[i]-v->getX(i));
     }
-    else {
-        cout << "false" << endl;
-    }
-    if (test4!=test)
-    {
-        cout << "true" << endl;
-    }
-    else {
-        cout << "false" << endl;
-    }
-    test += test2;
-    cout << test;
-    test3 -= test2;
-    cout << test3;
-    return 0;
+    dst = sqrt(dst);
+    return dst;
 }
+
+// int main()
+// {
+//     vector<double> x;
+//     x.push_back(1); x.push_back(2);
+//     Vector test  = Vector(x, 2);
+//     Vector test2 = 10*test;
+//     Vector test3 = test + test2;
+//     Vector test4 = test - test;
+//     cout << test;
+//     cout << test2;
+//     cout << test3;
+//     cout << test4;
+//     if (test4==test)
+//     {
+//         cout << "true" << endl;
+//     }
+//     else {
+//         cout << "false" << endl;
+//     }
+//     if (test4!=test)
+//     {
+//         cout << "true" << endl;
+//     }
+//     else {
+//         cout << "false" << endl;
+//     }
+//     test += test2;
+//     cout << test;
+//     test3 -= test2;
+//     cout << test3;
+//     return 0;
+// }
 
--- tp3/vector.hxx
+++ tp3/vector.hxx
@@ -1,5 +1,9 @@
+#ifndef VECTOR
+#define VECTOR
+
 #include <vector>
-#include <iostream> 
+#include <iostream>
+
 using namespace std;
 
 class Vector
@@ -7,20 +11,33 @@ class Vector
 private:
     vector<double> x_;
     unsigned int dim_;
+
 public:
     Vector(vector<double> x, unsigned int dim);
+    Vector(const Vector &v);
+    Vector();
     ~Vector();
-    Vector &operator*(double lamda);
+
+    Vector operator*(double lamda);
     friend Vector operator*(double lamda, const Vector &v);
-    Vector &operator+(const Vector &v);
-    Vector &operator-(const Vector &v);
+    Vector operator+(const Vector &v);
+    Vector operator-(const Vector &v);
+
     Vector &operator+=(const Vector &v);
     Vector &operator-=(const Vector &v);
+
     bool operator==(const Vector &v) const;
     bool operator!=(const Vector &v) const;
+
     Vector &operator=(const Vector &v);
+
     friend ostream &operator<<(ostream &out, const Vector &v);
+
     double getX(unsigned int i) const {return this->x_[i];};
     unsigned int getDim() const {return this->dim_;};
     void setX(unsigned int i, double val) {this->x_[i] = val;};
+
+    double distance(Vector *v);
 };
+
+#endif
